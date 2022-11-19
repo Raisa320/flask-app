c9adb8ef7cc9fe30b722c1515406c86d5ca4f01f
Raisa320 <67403486+Raisa320@users.noreply.github.com>
2022-11-18 23:45:26 -0500

    shell import

--- app/__init__.py
+++ app/__init__.py
@@ -19,3 +19,8 @@ login.login_view='login'
 login.login_message='Por favor inicie sesión para acceder a esta página.'
 from app import routes
 from app.models import User,Posts,Role
+
+#IMPORTA A LA SHEL AUTOMATICAMENTE LOS MODELOS
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Posts}
